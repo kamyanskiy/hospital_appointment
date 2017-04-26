ff4a5afab4bca85443b4b25a81a76bb5ad049792
Alexander Kamyanskiy <kamyanskiy@gmail.com>
2017-04-26 15:41:50 +0300

    play with staticfiles

--- hospital/settings.py
+++ hospital/settings.py
@@ -15,7 +15,6 @@ import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -32,7 +31,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'datetimewidget',
-    'bootstrap_themes',
+    #'bootstrap_themes',
     'appointment_form.apps.AppointmentFormConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -119,10 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
@@ -130,7 +127,6 @@ DATABASES['default'].update(db_from_env)
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
